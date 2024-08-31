c3b032912cdc924632f3059ff5b5dcc326be0790
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-08-31 17:24:58 +0300

    add user

--- Lab-Day 2/day2/src/auth/signup/signup.service.ts
+++ Lab-Day 2/day2/src/auth/signup/signup.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { SignUpDTO } from '../dto/auth.dto';
+import { User } from 'src/dto/auth.dto';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SignupService {
+    constructor(@InjectModel(User.name) private userModel: Model<User> ){}
 
     addUser(body: SignUpDTO){
-        console.log(body)
-        return "hello from signup"
+
+        this.userModel.insertMany(body)
+        return "user created"
     }
 }
